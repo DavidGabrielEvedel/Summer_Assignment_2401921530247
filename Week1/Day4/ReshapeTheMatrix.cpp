809dc68e06aa9965d2623318463c50a5c3cca176
DavidGabrielEvedel <siddharthsingh.azr@gmail.com>
2026-06-29 14:32:09 +0530

    Adding Repos

---
+++ Week1/Day4/ReshapeTheMatrix.cpp
@@ -0,0 +1,15 @@
+// Solution for Reshape the matrix
+class Solution {
+public:
+    vector<vector<int>> matrixReshape(vector<vector<int>>& mat, int r, int c) {
+        int m = mat.size(), n = mat[0].size();
+        if (m * n != r * c) return mat;     // invalid reshape, return original
+
+        vector<vector<int>> result(r, vector<int>(c));
+        for (int i = 0; i < m * n; i++) {
+            result[i / c][i % c] = mat[i / n][i % n];
+        }
+        return result;
+    }
+};
+
