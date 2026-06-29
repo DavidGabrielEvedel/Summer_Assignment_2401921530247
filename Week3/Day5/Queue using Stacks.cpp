cef754064cac9c8aab12d335966c2886dd3522bb
DavidGabrielEvedel <siddharthsingh.azr@gmail.com>
2026-06-30 00:08:09 +0530

    Implement queue using two stacks

---
+++ Week3/Day5/Queue using Stacks.cpp
@@ -0,0 +1,25 @@
+class MyQueue {
+    stack<int> inStack, outStack;
+public:
+    void push(int x) {
+        inStack.push(x);
+    }
+    int pop() {
+        peek();
+        int val = outStack.top();
+        outStack.pop();
+        return val;
+    }
+    int peek() {
+        if (outStack.empty()) {
+            while (!inStack.empty()) {
+                outStack.push(inStack.top());
+                inStack.pop();
+            }
+        }
+        return outStack.top();
+    }
+    bool empty() {
+        return inStack.empty() && outStack.empty();
+    }
+};
