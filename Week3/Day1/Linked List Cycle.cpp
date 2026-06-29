0b8c23de92ba84d60070746ce0bdb8587b15a9c5
DavidGabrielEvedel <siddharthsingh.azr@gmail.com>
2026-06-29 23:31:30 +0530

    Implement function to detect cycle in linked list

---
+++ Week3/Day1/Linked List Cycle.cpp
@@ -0,0 +1,10 @@
+bool hasCycle(ListNode *head) {
+    if (!head || !head->next) return false;
+    ListNode *slow = head, *fast = head;
+    while (fast && fast->next) {
+        slow = slow->next;
+        fast = fast->next->next;
+        if (slow == fast) return true;
+    }
+    return false;
+}
