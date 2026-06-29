18e334332cc8b025ed166be6c64e8e2b4a6f3a5c
DavidGabrielEvedel <siddharthsingh.azr@gmail.com>
2026-06-29 23:21:31 +0530

    Implement strStr() function for substring search

---
+++ Week2/Day3/Implement strStr().cpp
@@ -0,0 +1,8 @@
+int strStr(string haystack, string needle) {
+    int n = haystack.size(), m = needle.size();
+    if (m == 0) return 0;
+    for (int i = 0; i <= n - m; i++) {
+        if (haystack.substr(i, m) == needle) return i;
+    }
+    return -1;
+}
